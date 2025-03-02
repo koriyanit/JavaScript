217f7b4c68c79a02dc54d0d775952a502f5c0904
nit <nitkoriya6@.com>
2025-03-02 14:44:38 +0530

    datatype

---
+++ basic_1/02_datatype.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using node.js . not brower
+console.log(3+3);// readbilty is low
+
+console.log(3 == 3)
+// numb er =. 2 to power 53
+// bigint =>
+// string =? ""
+// boolean =>
+// null =>standalone balue
+// undefined 
