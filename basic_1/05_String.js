10a52f2298288a8ac9e208b11209b894eb67cb74
nit <nitkoriya6@.com>
2025-03-03 01:08:57 +0530

    learn about strings and math problems

---
+++ basic_1/05_String.js
@@ -0,0 +1,34 @@
+// const str = "nitkoriya"
+// console.log(str)
+// const str2 = "6@gmail"
+// const str3 = ".com"
+//  console.log(`my name is nit and my email is ${str}${str2} ${str3}`); 
+
+// const game = new String('nit koriya')
+// console.log(game)
+// console.log(game[4])
+// console.log(game.length)
+// console.log(game.toUpperCase())
+
+// const newstr = game.substring(0,3)
+// console.log(newstr)
+// const newStr = "      nit koriya _harsha_koriya    ";
+// console.log(newStr);
+// console.log(newStr.trim())
+// console.log(newStr.replace('n','t'))
+// console.log(newStr.includes('r'))
+// console.log(newStr.split('_'))
+// const handred = 1000000
+// console.log(handred.toLocaleString());
+
+//++++++++++++++++ Math ++++++++++++++++++
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.5));
+// console.log(Math.floor(3.5));
+// console.log(Math.ceil(-4.55));
+// console.log(Math.random());
+
+// max - min concept 
+min = 10
+max = 20
+console.log(Math.floor(Math.random() *( max - min +1)) + min);
---
+++ basic_1/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+onst str = "nitkoriya"
+// console.log(str)
+// const str2 = "6@gmail"
+// const str3 = ".com"
+//  console.log(`my name is nit and my email is ${str}${str2} ${str3}`); 
