4c8fb55b8b73782051da1f0b1a0c8ede13f45e6e
koriyanit <nitkoriya6@gmail.com>
2025-03-04 23:18:36 +0530

    ...

---
+++ basic_1/07_DatesInJs.js
@@ -0,0 +1,4 @@
+// myDate = new Date.now();
+// console.log(myDate.toLocaleString());
+// console.log(myDate);
+console.log(new Date.now);
--- basic_1/tempCodeRunnerFile.js
+++ basic_1/tempCodeRunnerFile.js
@@ -1,5 +1,2 @@
-onst str = "nitkoriya"
-// console.log(str)
-// const str2 = "6@gmail"
-// const str3 = ".com"
-//  console.log(`my name is nit and my email is ${str}${str2} ${str3}`); 
+myDate = new Date.now();
+// console.log(myDate.toLocaleString());
