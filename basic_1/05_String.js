1cea863b38ed4fd04252b408a880a8afb8d7303f
koriyanit <129496440+koriyanit@users.noreply.github.com>
2025-06-15 13:59:01 +0000

    just changes

--- basic_1/05_String.js
+++ basic_1/05_String.js
@@ -2,7 +2,7 @@
 // console.log(str)
 // const str2 = "6@gmail"
 // const str3 = ".com"
-//  console.log(`my name is nit and my email is ${str}${str2} ${str3}`); 
+//  console.log(`my name is nit and my email is ${str}${str2} ${str3}`);  String interpulation method 
 
 // const game = new String('nit koriya')
 // console.log(game)
