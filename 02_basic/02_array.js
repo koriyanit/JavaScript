e602188845618413f065f0514d7154479d5ba2d3
koriyanit <129496440+koriyanit@users.noreply.github.com>
2025-06-16 08:31:04 +0000

    Learn about array

---
+++ 02_basic/02_array.js
@@ -0,0 +1,20 @@
+const mh = ["thor", "ironman", "captain"]
+const dh = ["superman", "batman", "flash"]
+
+// const newHero = mh.concat(dh)
+// mh.push(dh)
+// console.log(newHero)
+// const all_new_heros = [...mh, ...dh]
+// console.log(all_new_heros)
+
+// const aa = [1,2,3,[4,5,6,5],7,[34,5]]
+// console.log(aa)
+// const an = aa.flat(Infinity)
+// console.log(an)
+
+
+// console.log(Array.isArray("NItkoriya"))
+// console.log(Array.from("NItkoriya"))
+// console.log(Array.from({name : "NItkoriya"}))
+
+
