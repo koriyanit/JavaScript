979aa9e3db16150c23b0658481b5a11686eae690
koriyanit <129496440+koriyanit@users.noreply.github.com>
2025-06-16 09:02:58 +0000

    learn objects

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,12 @@
+const tempuser = {
+    name: "nit",
+    age : 23,
+    location : "kodinar",
+    email : "nitkoriya@.com"
+}
+
+// console.log(tempuser["email"])
+// console.log(tempuser.email)
+// tempuser.email = "koriya@.com"
+// console.log(tempuser["email"])
+// Object.freeze(tempuser)
