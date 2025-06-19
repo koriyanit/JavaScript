2dcdf6769aed873c4cd278c0b9dfd131a039608e
koriyanit <129496440+koriyanit@users.noreply.github.com>
2025-06-19 18:17:22 +0000

    about arrowfunction

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,23 @@
+// console.log(this)
+// const user = {
+//     username: "nit",
+//     price: "800",
+//     welcomeMessage: function one(username) {
+//                           console.log(this.username)
+//                           return`${this.username}  welcome to user`
+
+//                     }
+       
+// }
+
+// console.log(user.welcomeMessage())
+
+// Arrow function
+const addTwo = (n1,n2) => {
+    return n1+n2
+}
+// implicit
+const addonw =(n1, n2) => n1+n2
+
+console.log(addTwo(1,2))
+console.log(addone(1,2))
