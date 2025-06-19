aad4de377b338e17b8d5e229f56ad163a4861abf
koriyanit <129496440+koriyanit@users.noreply.github.com>
2025-06-19 10:40:23 +0000

    functions talks

---
+++ 03_basic/01_function.js
@@ -0,0 +1,29 @@
+// function sum(num1, num2) {
+//     return num1+num2;
+// }
+// console.log("result is ",sum(23,23));
+
+
+// function loginUser(username) {
+//     return `${username} just logged in `
+// }
+
+// console.log(loginUser("nit"))
+// function calculateCart( ...num1) {
+//     return num1
+// }
+// console.log(calculateCart(23,434,45,443))
+
+// const user = {
+//     username: "nit",
+//     price: 199
+// }
+// function handleObject(anyobject) {
+//     console.log(`user is ${anyobject.username} and price is${anyobject.price}`);
+// }
+
+// handleObject(user);
+// handleObject({
+//     username: "koriya",
+//     price: 300
+// })
---
+++ 03_basic/02_scope.js
@@ -0,0 +1,12 @@
+if(true) {
+let a = 10
+const b = 20 
+ var c = 50
+}
+
+//  console.log(a);
+//  console.log(b)
+ console.log(c)
+ 
+ // var key world has globle scope 
+ 
